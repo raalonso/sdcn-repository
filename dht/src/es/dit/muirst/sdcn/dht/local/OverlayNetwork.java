d12a29926ccb2c39dc926880750c058ded1cff92
alonso <ramon.alonsogarcia@gmail.com>
2017-01-07 20:47:03 +0100

    Added method printOutDHT().

--- dht/src/es/dit/muirst/sdcn/dht/local/OverlayNetwork.java
+++ dht/src/es/dit/muirst/sdcn/dht/local/OverlayNetwork.java
@@ -1,6 +1,7 @@
 package es.dit.muirst.sdcn.dht.local;
 
 import es.dit.muirst.sdcn.dht.interfaces.Node;
+import es.dit.muirst.sdcn.dht.interfaces.DHT;
 
 import java.util.ArrayList;
 
@@ -14,7 +15,17 @@ public class OverlayNetwork {
 
     public void printOutNetwork() {
         System.out.println("\n-- Overlay Network -----");
-        nodes.forEach(System.out::println);
+        for (Node item: this.nodes)
+            System.out.println(item);
+        // nodes.forEach(System.out::println);
+        System.out.println("------------------------\n");
+    }
+
+    public void printOutDHT() {
+        System.out.println("\n-- Distributed Hash Table (DHT) -----");
+        for (Node item: this.nodes)
+            System.out.println(((DHT) item).printOutLocalDHT());
+        // nodes.forEach(System.out::println);
         System.out.println("------------------------\n");
     }
 
