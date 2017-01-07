90d3061e673eb828c9834fce5befec2aab255860
alonso <ramon.alonsogarcia@gmail.com>
2017-01-07 20:40:06 +0100

    Added/modified leave methods.

--- dht/src/es/dit/muirst/sdcn/dht/interfaces/Node.java
+++ dht/src/es/dit/muirst/sdcn/dht/interfaces/Node.java
@@ -1,26 +1,31 @@
 package es.dit.muirst.sdcn.dht.interfaces;
 
 import es.dit.muirst.sdcn.dht.StateTable;
+import es.dit.muirst.sdcn.dht.messaging.Message;
+
 import java.util.Hashtable;
 
 public interface Node <Address> {
 
     // Init Pastry
     //
-    int initPastry(Node nearbyNode, int key);
+    int initPastry(Node bootstrapNode, int key);
 
     int getNodeId();
     Address get(int key);
 
     // Core routing algorithm
     //
-    void route(String msg, int key);
+    void route(Message msg, int key);
 
     // Operations
     //
     StateTable join(Node node);
-    void leave();
+    void leave(Node fromNode, int nodeId, StateTable stateTable);
     void broadcastState(Node fromNode, StateTable stateTable);
+
+    //
+    void onNodeLeave(int nodeId);
 
     // Internal operations
     //
